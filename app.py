725fbfa94bb99b74681fec1bf568ff98a8c73285
fabiano-githu <chaviersantos70@gamil.com>
2026-05-29 14:37:23 -0300

    Primeiros Testes

---
+++ app.py
@@ -0,0 +1,21 @@
+
+nome = input ("Qual seu nome?")
+print("Ola  mundo!")
+
+# Atribui um outro nome a variável
+nome = "Claudenilson"
+
+# String vazia
+nome = ""
+
+
+nome = None
+
+# Apaga uma variével 
+# del nome
+
+# Concatenando texto com variével
+print("Meu nome " + nome + "!")
+
+
+
